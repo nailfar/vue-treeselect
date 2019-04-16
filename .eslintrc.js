f6141f036f0d8efcc6ea991514844d5c71edd1a9
Wangxd <nailfar@qq.com>
2019-04-16 10:22:40 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  extends: ["riophae/vue"],
+  plugins: ["react"],
+  globals: {
+    PKG_VERSION: true,
+  },
+  settings: {
+    "import/resolver": {
+      node: null,
+      webpack: {
+        config: "build/webpack.base.conf.js",
+      },
+    },
+  },
+  rules: {
+    "import/exports-last": 0,
+    "import/no-named-as-default": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react/jsx-uses-vars": 2,
+    "vue/attributes-order": 0,
+    "vue/no-v-html": 0,
+    "no-confusing-arrow": 0,
+    "no-console": 0,
+    "no-extra-parens": 0,
+    "no-warning-comments": 0,
+    "no-undefined": 0,
+    "prefer-destructuring": 0,
+  },
+};
